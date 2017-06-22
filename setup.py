b0fcf8f27027593ed68850d1b798f0fd9e05ad99
Jason Banich <Jdban101@gmail.com>
2017-06-21 21:16:05 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='joke_module',
+    version='0.1.0',
+    description='Test description',
+    url='http://github.com/jdban/my-test-pip-module',
+    author='Jason B',
+    author_email='fake@example.com',
+    packages=['joke_module'],
+    zip_safe=False)
